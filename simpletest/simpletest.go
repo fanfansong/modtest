74373ff980498b51a78a4c8b1df3386b86155e99
宋慧琳 <fanfansong@songhuilindeMacBook-Pro.local>
2019-07-16 15:59:19 +0800

    v2.0

--- simpletest/simpletest.go
+++ simpletest/simpletest.go
@@ -2,9 +2,16 @@ package simpletest
 
 import "github.com/apex/log"
 
-func String() {
+// func String() {
+// 	log.WithFields(log.Fields{
+// 		"package":  "simpletest",
+// 		"function": "String()",
+// 	}).Info("info")
+// }
+
+func String2() {
 	log.WithFields(log.Fields{
 		"package":  "simpletest",
-		"function": "String()",
+		"function": "String2()",
 	}).Info("info")
 }
